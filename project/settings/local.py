0d91bb85241ae47a166c9e95c83a593d8226fcb2
Saif Eldin Ayman <saifayman3021@gmail.com>
2026-03-19 22:54:22 +0200

    Update local.py for local development settings
    Comment out the email backend setting and add Celery task settings for local development.

--- project/settings/local.py
+++ project/settings/local.py
@@ -10,7 +10,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Use console email backend in development
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Disable security features in development
 SECURE_SSL_REDIRECT = False
@@ -21,6 +21,12 @@ SECURE_HSTS_SECONDS = 0
 # CORS for local development
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:3000,http://localhost:5173,http://localhost:8000").split(",")
 CORS_TRUSTED_ORIGINS = os.getenv("CORS_TRUSTED_ORIGINS", "http://localhost:3000,http://localhost:5173,http://localhost:8000").split(",")
+
+# TODO: Remove these settings in production
+# Execute tasks locally (synchronously) instead of sending them to the broker
+CELERY_TASK_ALWAYS_EAGER = True
+# If an eager task raises an exception, propagate it to the view
+CELERY_TASK_EAGER_PROPAGATES = True
 
 # Enable Django Debug Toolbar (if installed)
 try:
